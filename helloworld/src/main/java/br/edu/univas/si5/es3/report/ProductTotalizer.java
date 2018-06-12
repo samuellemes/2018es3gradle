0b3186ce9a82f0378e79ba72aec237c34f4744b0
Samuel Lemes <samuellemes25@gmail.com>
2018-06-12 20:32:06 -0300

    Adding tests..

--- helloworld/src/main/java/br/edu/univas/si5/es3/report/ProductTotalizer.java
+++ helloworld/src/main/java/br/edu/univas/si5/es3/report/ProductTotalizer.java
@@ -3,7 +3,8 @@ package br.edu.univas.si5.es3.report;
 import java.util.List;
 
 public class ProductTotalizer {
-	private ProductDAO dao;
+
+	private ProductDAO dao = new ProductDAO();
 
 	public int totalize() {
 		List<Product> products = dao.listAll();
---
+++ helloworld/src/test/java/br/edu/univas/si5/es3/report/ProductTotalizerTest.java
@@ -0,0 +1,54 @@
+package br.edu.univas.si5.es3.report;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ProductTotalizerTest {
+
+	@Mock
+	private ProductDAO dao;
+
+	@InjectMocks
+	private ProductTotalizer totalizer;
+
+	@Test
+	public void totalizerWithTwoProjectsShouldSumQuantities() {
+		// given
+		Product p1 = new Product();
+		p1.setQuantity(5);
+
+		Product p2 = new Product();
+		p2.setQuantity(4);
+		when(dao.listAll()).thenReturn(Arrays.asList(p1, p2));
+
+		// when
+		int result = totalizer.totalize();
+
+		// then
+		assertThat(result, equalTo(9));
+	}
+
+	@Test
+	public void totalizeWhitEmptyArrayShouldReturnZero() {
+		// given
+		when(dao.listAll()).thenReturn(new ArrayList<>());
+
+		// when
+		int result = totalizer.totalize();
+
+		// then
+		assertThat(result, equalTo(0));
+	}
+
+}
