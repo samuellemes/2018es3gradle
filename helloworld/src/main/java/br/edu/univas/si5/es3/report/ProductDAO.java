840edb2fa070fcd685fa335eb81ff7b2967f6e4d
Samuel Lemes <samuellemes25@gmail.com>
2018-06-12 19:55:19 -0300

    Create Product Totalizer class.

---
+++ helloworld/src/main/java/br/edu/univas/si5/es3/report/ProductDAO.java
@@ -0,0 +1,10 @@
+package br.edu.univas.si5.es3.report;
+
+import java.util.List;
+
+public class ProductDAO {
+
+	public List<Product> listAll() {
+		return null;
+	}
+}
---
+++ helloworld/src/main/java/br/edu/univas/si5/es3/report/ProductTotalizer.java
@@ -0,0 +1,12 @@
+package br.edu.univas.si5.es3.report;
+
+import java.util.List;
+
+public class ProductTotalizer {
+	private ProductDAO dao;
+
+	public int totalize() {
+		List<Product> products = dao.listAll();
+		return products.stream().mapToInt(Product::getQuantity).sum();
+	}
+}
